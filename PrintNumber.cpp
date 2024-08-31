200bf43b21bc3d1c4eba0bc2dbd5b4a2e29a2e75
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-08-31 20:03:08 +0530

    new commit

---
+++ PrintNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+// In C++, iostream stands for input/output stream. It's a header file that provides functionality for reading and writing data to and from standard input (keyboard) and standard output (screen).
+
+using namespace std;
+// this is use to short the code kyuki agar ya nahi use karenga to huma har bar cin cout ma std:: use karma padega
+int main(){
+
+    // sara logic {} curly bracket ka andar likhna hota ha
+
+    cout<<"98";
+    // dont forget semicolon ;
+    // cout hum screen pa output show karna ka leya use karta ha
+
+    return 0;
+    // return karna is important
+}
