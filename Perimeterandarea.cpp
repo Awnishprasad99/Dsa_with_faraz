40961f147ad3faa427a597361fedb18e3d40c8c9
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-09-03 20:58:44 +0530

    area and perimeter

---
+++ Perimeterandarea.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+// cout<<"Enter the rectangle length and breath"<<endl;
+
+// cout<<"Enter the value of l"<<" ";
+// int l;
+// cin>>l;
+
+// cout<<"Enter the value of b "<<" ";
+// int b;
+// cin>>b;
+
+// cout<<"The area of rectangle is "<<" ";
+// cout<<(l*b);
+
+
+// cout<<"Enter the value of side to find "<<endl;
+
+// int s;
+// cin>>s;
+
+// cout<<"The are of square is"<<pow(s,2);
+
+// cout<<"Enter the value of side to find out perimeter of square";
+// int s;
+// cin>>s;
+
+// cout<<"the perimeter of square is "<<(4*s);
+
+
+// cout<<"Enter the value of l and b to find the perimeter of rectangle"<<endl;
+
+// cout<<"Enter the length of square"<<" ";
+
+// int l;
+// cin>>l;
+
+// cout<<"Enter the value of breath"<<" ";
+// int b;
+// cin>>b;
+
+// cout<<"The perimeter of square is"<<" "<<2*(l+b);
+
+
+    return 0;
+}
