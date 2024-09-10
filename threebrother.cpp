5f9fc51f4f53eafa4935f20e8e2bd62ac4efaabf
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-09-10 20:46:33 +0530

    new commit

---
+++ threebrother.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    if((a==1&&b==2)||(b==1&&a==2)){
+        cout<<"3";
+        
+    }
+    else cout<<""
+}
--- twobrotherproblem.cpp
+++ twobrotherproblem.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -8,10 +9,14 @@ int main(){
   if(a==1&&b==2){
     cout<<"3";
   }
+  else if(a==3&&b==1){
+    cout<<"2";
+  } 
   else if(a==1&&b==3){
    cout<<"2"; 
   }
   else if(a==2&&b==3){
     cout<<"1";
   }
+
 }
